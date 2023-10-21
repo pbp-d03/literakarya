04f74e61034885b1bce6fb2cc2509641be4738c8
muhkemallgp <muhkmuhkemallathifgp@gmail.com>
2023-10-22 01:04:35 +0700

    Inisial Navbar

--- book_page/urls.py
+++ book_page/urls.py
@@ -4,5 +4,5 @@ from book_page.views import get_books
 app_name = "book_page"
 
 urlpatterns = [
-    path('api/', get_books,name="get_books"), # ini buat nampilin api/buku
+    path('api/', get_books,name="get_books"), # ini buat nampilin api (json buku)
 ]
