5c74d8078012591183f843dd54ac2f32790944fb
kimiagastya <kimiagastya@gmail.com>
2023-10-23 23:18:26 +0700

    Everything forum related

--- forum/views.py
+++ forum/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def show_main(request):
+    return render(request, "forum.html")
--- project_literakarya/urls.py
+++ project_literakarya/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')), # HOMEPAGE NANTI
-    path('book-pages/',include("book_page.urls")) # dapatin Buku
+    path('book-pages/',include("book_page.urls")), # dapatin Buku
+    path('forum/',include("forum.urls")),
 ]
