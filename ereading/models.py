d04d528dd968abb5f3b92046e3fe8aa2f960beac
Kristoforus Adi Himawan <kristoforus.adi28@gmail.com>
2023-10-24 19:44:55 +0700

    Membuat dashboard dan model ereading

--- ereading/models.py
+++ ereading/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, AnonymousUser
 
 class Ereading(models.Model):
     title = models.CharField(max_length=100)
@@ -7,4 +7,4 @@ class Ereading(models.Model):
     description = models.TextField()
     link = models.URLField()
     last_updated = models.DateTimeField(auto_now=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # user = models.ForeignKey(User, on_delete=models.CASCADE, default = AnonymousUser().id)
--- ereading/urls.py
+++ ereading/urls.py
@@ -4,5 +4,7 @@ from ereading.views import *
 app_name = 'ereading'
 
 urlpatterns = [
-    path('', show_dashboard, name='show_dashboard')
+    path('', show_dashboard, name='show_dashboard'),
+    path('get-json/', get_json, name='get_json'),
+    path('add-ereading/', add_ereading, name='add_ereading')
 ]
