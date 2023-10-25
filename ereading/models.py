57b884c9cf0a84fd1803ed74c4ce5f60cebb6dba
Kristoforus Adi Himawan <kristoforus.adi28@gmail.com>
2023-10-26 06:58:59 +0700

    Form control dan AJAX DELETE

--- ereading/models.py
+++ ereading/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User, AnonymousUser
+from django.contrib.auth.models import User
 
 class Ereading(models.Model):
     title = models.CharField(max_length=100)
@@ -7,4 +7,4 @@ class Ereading(models.Model):
     description = models.TextField()
     link = models.URLField()
     last_updated = models.DateTimeField(auto_now=True)
-    # user = models.ForeignKey(User, on_delete=models.CASCADE, default = AnonymousUser().id)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
