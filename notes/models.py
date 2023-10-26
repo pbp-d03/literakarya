37cc4899efd734f6691c37d75c951a956db08588
shantiyoga <shantiyogars@gmail.com>
2023-10-26 18:44:43 +0700

    notes

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    date = models.DateTimeField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    judul_catatan = models.CharField(max_length=30)
+    judul_buku = models.CharField(max_length=255)
+    catatan = models.TextField()
+    penanda = models.TextField()
+    
---
+++ notes/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include
+from forum.views import show_forum, add_post, add_reply, delete_post, delete_reply, get_posts_json, get_replies_json
+
+app_name = "notes_page"
+
+urlpatterns = [
+    path('', show_notes,name="show_notes"),
+    path('add-post/', add_post, name='add_post'),
+    path('add-reply/<int:id>', add_reply, name='add_reply'),
+    path('delete-post/<int:id>', delete_post, name='delete_post'),
+    path('delete-reply/<int:id>', delete_reply, name='delete_reply'),
+    path('json/all-posts/', get_posts_json, name="get_posts"),
+    path('json/all-replies/<int:id>', get_replies_json, name="get_replies"),
+]
