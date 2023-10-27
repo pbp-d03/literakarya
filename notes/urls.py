021d596f15bc17be3bab5460f428a4198a11e6a6
shantiyoga <shantiyogars@gmail.com>
2023-10-28 00:04:20 +0700

    notes

--- notes/urls.py
+++ notes/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from notes.views import (show_notes, add_note, delete_note, edit_note, create_note, get_notes_json)
+from notes.views import (show_notes, add_note, delete_note, edit_note, get_notes_json)
 
 app_name = "notes_page"
 
@@ -7,7 +7,6 @@ urlpatterns = [
     path('', show_notes, name='show_notes'),
     path('add-note/', add_note, name='add_note'),
     path('edit-note/<int:id>', edit_note, name='edit_note'),
-    path('create-note/', create_note, name='create_note'),
     path('delete-note/<int:id>', delete_note, name='delete_note'),
     path('get-notes/', get_notes_json, name="get_notes_json"),
 
--- notes/views.py
+++ notes/views.py
@@ -13,11 +13,7 @@
 
 from .models import Note
 from book_page.models import Book
-from django.shortcuts import redirect
 from notes.forms import NoteForm
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib import messages  
-from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.shortcuts import render
@@ -39,18 +35,27 @@ def show_notes(request):
     }
     return render(request, "notes.html", context)
 
-def create_note(request):
-    form = NoteForm(request.POST or None)
-
-    if form.is_valid() and request.method == "POST":
-        note = form.save(commit=False)
-        note.user = request.user
-        form.save()
-        return HttpResponseRedirect(reverse('notes:show_notes'))
-
-    context = {'form': form}
-    return render(request, "create_note.html", context)
-
+@csrf_exempt         
+def add_note(request):
+    if request.method == 'POST':
+        judul_catatan = request.POST.get("judul_catatan")
+        judul_buku = request.POST.get("judul_buku")
+        konten_catatan = request.POST.get("konten_catatan")
+        penanda = request.POST.get("penanda")
+        user=request.user
+        
+        new_note = Note(
+            user=user,
+            judul_catatan=judul_catatan,
+            judul_buku=judul_buku,
+            konten_catatan=konten_catatan,
+            penanda=penanda,
+        )
+        
+        new_note.save()
+        return HttpResponse(b"CREATED", status=201)
+    
+    return HttpResponseNotFound()
 
 def get_notes_json(request):
     note_item = Note.objects.all()
@@ -62,43 +67,14 @@ def edit_note(request, id):
 
     if form.is_valid() and request.method == "POST":
         form.save()
-        return HttpResponseRedirect(reverse('notes\:show_notes'))
+        return HttpResponseRedirect(reverse('notes:show_notes'))
 
     context = {'form': form}
     return render(request, "edit_note.html", context)
-
-# AJAX related
-
-@csrf_exempt         
-def add_note(request):
-    if request.method == 'POST':
-        judul_catatan = request.POST.get("judul_catatan")
-        judul_buku = request.POST.get("judul_buku")
-        konten_catatan = request.POST.get("konten_catatan")
-        penanda = request.POST.get("penanda")
-        date_added = timezone.now()
-        user=request.user
-        
-        new_note = Note(
-            user=user,
-            judul_catatan=judul_catatan,
-            judul_buku=judul_buku,
-            konten_catatan=konten_catatan,
-            penanda=penanda,
-            date_added=date_added,
-        )
-        
-        new_note.save()
-        return HttpResponse(b"CREATED", status=201)
-    
-    return HttpResponseNotFound()
-
 
 @csrf_exempt
 def delete_note(request, id):
     if request.method == "DELETE":
         note = Note.objects.get(pk=id)
         note.delete()
-        return HttpResponse(b"DELETED", status=201)
-    
-    return HttpResponseNotFound()
+        return HttpResponseRedirect(reverse('notes:show_notes'))
