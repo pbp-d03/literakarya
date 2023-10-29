d0ead2aaefac0a8c010b5fc6627e80007ad5d05a
kimiagastya <kimiagastya@gmail.com>
2023-10-29 18:13:51 +0700

    Add 3 test, modify default date di models

--- forum/models.py
+++ forum/models.py
@@ -1,3 +1,4 @@
+from django.utils import timezone
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -8,10 +9,10 @@ class Post(models.Model):
     subject = models.CharField(max_length=255)
     topic = models.CharField(max_length=255, null=False)
     message = models.TextField()
-    date = models.DateTimeField()
+    date = models.DateTimeField(default=timezone.now)
     
 class Reply(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete = models.CASCADE, related_name="reply")
     body = models.TextField()
-    date = models.DateTimeField()
+    date = models.DateTimeField(default=timezone.now)
