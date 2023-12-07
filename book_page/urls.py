c2a4d6f1b89da5455628e6b6547e9ed628d0ca03
muhkemallgp <muhkmuhkemallathifgp@gmail.com>
2023-12-07 11:03:03 +0700

    edit komen

--- book_page/urls.py
+++ book_page/urls.py
@@ -7,16 +7,16 @@ app_name = "book_page"
 urlpatterns = [
     path('api/', get_books,name="get_books"), # ini buat nampilin api (json buku)
     path('',show_list_books,name="show_list_books"),
-    path('book/<int:id>', show_book, name = "show_book"),
+    path('book/<int:id>/', show_book, name = "show_book"),
 
-    path('bookmark', show_bookmark, name = "show_bookmark"),
+    path('bookmark/', show_bookmark, name = "show_bookmark"),
 
-    path('add-komen/<int:id1>', add_comment_ajax, name = "add_comment_ajax"),
-    path('get-komen/<int:id1>', get_komen_json, name='get_komen_json'),
+    path('add-komen/<int:id1>/', add_comment_ajax, name = "add_comment_ajax"),
+    path('get-komen/<int:id1>/', get_komen_json, name='get_komen_json'),
 
-    path('add-bookmark/<int:id>', add_bookmark_ajax, name = "add_bookmark_ajax"),
-    path('delete-bookmark/<int:id>', delete_bookmark, name = "delete_bookmark"),
+    path('add-bookmark/<int:id>/', add_bookmark_ajax, name = "add_bookmark_ajax"),
+    path('delete-bookmark/<int:id>/', delete_bookmark, name = "delete_bookmark"),
 
-    path('add-likes/<int:id>', add_likes, name = "add_likes"),
-    path('delete-komen/<int:id>', delete_komen, name = "delete_komen"),
+    path('add-likes/<int:id>/', add_likes, name = "add_likes"),
+    path('delete-komen/<int:id>/', delete_komen, name = "delete_komen"),
 ]
