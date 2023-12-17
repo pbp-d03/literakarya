277f9a542d0b90bc19a58478e9b3d9c5af7f0b48
Kristoforus Adi Himawan <kristoforus.adi28@gmail.com>
2023-12-17 13:18:43 +0700

    Add flutter dan set TIME_ZONE

--- ereading/urls.py
+++ ereading/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('get-json/', get_json, name='get_json'),
     path("get-json/<str:uname>/",get_json_user, name="get_json_user"),
     path('add-ereading/', add_ereading, name='add_ereading'),
+    path('add-ereading-flutter/', add_ereading_flutter, name='add_ereading_flutter'),
     path('delete-ereading/', delete_ereading, name='delete_ereading'),
     path('accept-ereading/', accept_ereading, name='accept_ereading'),
     path('reject-ereading/', reject_ereading, name='reject_ereading'),
